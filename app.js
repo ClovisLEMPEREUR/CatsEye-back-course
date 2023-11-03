b0bac67c3768d98d001a8a17dac349c476f4d5e5
hlodowing <pohacu@gmail.com>
2023-11-03 15:57:09 +0100

    Gestion de la connexion corrigé

--- app.js
+++ app.js
@@ -10,12 +10,18 @@ app.listen(appport, () => {
 const mysql= require('mysql2');
 const datacnx=require('./databaseConnexion.json'); // ajoute tes informations de connexion dans le fichier databaseConnexion.json.
 const { host, port, user, password, database } = datacnx.database;
-const db = mysql.createConnection({ host, port, user, password, database });
 
-db.connect(err=>{
-    if(err) {
-        console.log(err,'dberr');
-        process.exit(err);
-    } else {console.log(`Tu es connecté à la base de données: ${database} ...`);}    
-});
+if (host!= "" && port!="" && user!="" && password!="" && database!=""){
+    const db = mysql.createConnection({ host, port, user, password, database });
+
+    db.connect(err=>{
+        if(err) {
+            console.log(err,'dberr');
+            process.exit(err);
+        } else {console.log(`Tu es connecté à la base de données: ${database} ... GOOD JOB !`);}    
+    });
+}else{
+    console.log("FATAL ERROR:","T'as oublié quelque chose !? - Ton fichier de connexion n'est pas correcte. :( ");
+    process.exit(1);
+}
 
